15322625e99cef5a8b92b52837836bbb1aa7d9be
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2024-08-23 11:53:47 -0700

    Modified the TaskItem Model

--- api/models.py
+++ api/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 # Create your models here.
 class TaskItem(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=200)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
-    #completed_at = models.DateTimeField(null=True, blank=True)
     
     
     def __str__(self):
