75baedb7259c46659d2749756c1f5d6875dfdfe2
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2024-08-23 11:49:21 -0700

    Modified TaskItem component to show task ID and task's created date. Added Delete Button to task item

--- frontend/src/components/TaskItem/TaskItem.jsx
+++ frontend/src/components/TaskItem/TaskItem.jsx
@@ -1,10 +1,25 @@
 import "./TaskItem.css"
 
-const TaskItem = ({ task }) => {
+const TaskItem = ({ task, endpoint, handleDelete }) => {
+
+
   return (
-    <div className='app-task-item'>
+    <div className={`app-task-item ${endpoint !== "tasks" && "project-pad"}`}>
+
+      {endpoint === "tasks" && (
+        <div className="task-header">
+          <p className="task-id">{task.id}</p>
+          <p className="task-created-date">Created On: {task.created_at.split("T")[0]}</p>
+        </div>
+      )}
       <h2 className="task-title">{task.title}</h2>
       <p className="task-desc">{task.description}</p>
+      {endpoint === "tasks" ? (
+        <button className="delete-btn" onClick={() => handleDelete(task.id)}>Delete Task</button>
+      ) : (
+        <p className="task-created-date">Created On: {task.created_at.split("T")[0]}</p>
+      )}
+      
     </div>
   )
 }
